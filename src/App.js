1c7f1b0f2fe1d9cd0194152fcd22cec707dd4dc3
mcgidoug <mcgidoug@gmail.com>
2023-01-23 13:07:43 -0600

    added demo links and photo grid

--- src/App.js
+++ src/App.js
@@ -13,13 +13,25 @@ function App() {
         <h4>Dark Art & Illustration</h4>
         <p>Commission work available with inquiry</p>
         <div className="icons">
-          <AiFillLinkedin />
-          <AiFillTwitterCircle />
-          <AiFillYoutube />
+          <a a href="https://www.youtube.com" target="blank">
+            <AiFillYoutube />
+          </a>
+          <a a href="https://www.youtube.com" target="blank">
+            <AiFillLinkedin />
+          </a>
+          <a a href="https://www.youtube.com" target="blank">
+            <AiFillTwitterCircle />
+          </a>
         </div>
       </div>
 
       <div className="portfolio">
+        <img src="/images/hooded-city.jpg" alt="" />
+        <img src="/images/invader.jpg" alt="" />
+        <img src="/images/three-on-three.jpg" alt="" />
+        <img src="/images/hooded-city.jpg" alt="" />
+        <img src="/images/invader.jpg" alt="" />
+        <img src="/images/three-on-three.jpg" alt="" />
         <img src="/images/hooded-city.jpg" alt="" />
         <img src="/images/invader.jpg" alt="" />
         <img src="/images/three-on-three.jpg" alt="" />
