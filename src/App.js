11fdaf0ceb8d997b28fbca29752315067f5bee2a
mcgidoug <mcgidoug@gmail.com>
2023-02-09 16:21:25 -0600

    separated pages into components and added react router dom for linking pages

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
 import "./App.css";
-import { AiFillInstagram } from "react-icons/ai";
-import { FaEtsy, FaBandcamp } from "react-icons/fa";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <nav className="navbar">
-        <a href="mailto:martinkralovec65@gmail.com">
-          <div className="contactButton">Contact</div>
-        </a>
-      </nav>
-      <div className="header">
-        <h1>Martin Kralovec</h1>
-        <h4>Dark Art & Illustration</h4>
-        <p>Commission Work Available with Inquiry</p>
-        <div className="icons">
-          <a a href="https://www.instagram.com/martin.kralovec/" target="blank">
-            <AiFillInstagram />
-          </a>
-          <a
-            a
-            href="https://www.etsy.com/shop/martinkralovecart/"
-            target="blank"
-          >
-            <FaEtsy />
-          </a>
-          <a a href="https://bladderboys.bandcamp.com/" target="blank">
-            <FaBandcamp />
-          </a>
-        </div>
-      </div>
-
-      <div className="portfolio">
-        <img src="/images/hooded-city.jpg" alt="" />
-        <img src="/images/invader.jpg" alt="" />
-        <img src="/images/three-on-three.jpg" alt="" />
-        <img src="/images/lookout.jpg" alt="" />
-        <img src="/images/burried.jpg" alt="" />
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I am an artist and illustrator located in Chicago. I work with pen and
+        ink mostly but explore all different types of media; including but not
+        limited to markers, watercolor, acrylic, and paint. I am stylistically
+        versatile, which allows me to reach a larger clientele. I use my art to
+        work out thoughts and convey emotion. I am inspired by googie art,
+        books, art nouveau, nature, gothic, horror art, and movies. From this, I
+        create projects, series, minicomics, and individual pieces of work.
+      </p>
+    </div>
+  );
+};
+
+export default About;
